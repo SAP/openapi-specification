82945ae4459421cc420ff63752b354403810ad6c
Simon Heimler <simon.heimler@sap.com>
2026-09-12 12:36:59 -0700

    Modernize package tooling
    Signed-off-by: Simon Heimler <simon.heimler@sap.com>

---
+++ src/generate-specs.mjs
@@ -0,0 +1,47 @@
+import { readFileSync, writeFileSync } from "node:fs";
+
+function readJson(relativePath) {
+  return JSON.parse(readFileSync(new URL(relativePath, import.meta.url), "utf8"));
+}
+
+const originalSchemas = {
+  "2.0": readJson("../original-schemas/v2.0.json"),
+  "3.0": readJson("../original-schemas/v3.0.json"),
+};
+
+const commonExtensions = readJson("../sap-extensions/extensions.json");
+const extensionSchemas = {
+  "2.0": readJson("../sap-extensions/v2.0-extensions.json"),
+  "3.0": readJson("../sap-extensions/v3.0-extensions.json"),
+};
+
+function getAtPath(value, objectPath) {
+  return objectPath.split(".").reduce((current, key) => current?.[key], value);
+}
+
+function applyExtension(sourceSchema, extensions) {
+  const destinationSchema = structuredClone(sourceSchema);
+
+  for (const { paths, properties } of extensions["x-extends"]) {
+    for (const objectPath of paths) {
+      const destinationObject = getAtPath(destinationSchema, objectPath);
+      if (destinationObject) {
+        Object.assign(destinationObject, properties);
+      }
+    }
+  }
+
+  Object.assign(destinationSchema.definitions, extensions.definitions);
+  return destinationSchema;
+}
+
+function generateSchema(version) {
+  const withCommonExtensions = applyExtension(originalSchemas[version], commonExtensions);
+  const schema = applyExtension(withCommonExtensions, extensionSchemas[version]);
+  const destination = new URL(`../sap-schemas/v${version}/schema.json`, import.meta.url);
+
+  writeFileSync(destination, JSON.stringify(schema, null, 2));
+}
+
+generateSchema("2.0");
+generateSchema("3.0");
